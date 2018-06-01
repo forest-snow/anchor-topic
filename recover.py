357781699a239d7fdf81223ee15d32c9ce6c96d0
Michelle Yuan <myuan@tlingit.umiacs.umd.edu>
2018-06-01 14:38:56 -0400

    start new models

---
+++ recover.py
@@ -0,0 +1,172 @@
+import scipy.sparse
+import scipy.stats
+import numpy 
+import random
+import multiprocessing.pool
+from numba import jit
+
+def logsum_exp(y):
+    m = y.max()
+    return m + numpy.log((numpy.exp(y - m)).sum())
+
+def KL(p,q):
+    return scipy.stats.entropy(p, q)        
+
+def KLSolveExpGrad(y,x,eps, alpha=None):
+    c1 = 10**(-4)
+    c2 = 0.9
+    it = 1 
+    
+    y = numpy.clip(y, 0, 1)
+    x = numpy.clip(x, 0, 1)
+
+    (k, n) = x.shape
+    mask = list(numpy.nonzero(y)[0])
+
+    y = y[mask]
+    x = x[:, mask]
+
+    x += 10**(-9)
+    x /= x.sum(axis=1)[:,newaxis]
+
+    if alpha == None:
+        alpha = numpy.ones(k)/k
+
+    old_alpha = numpy.copy(alpha)
+    log_alpha = numpy.log(alpha)
+    old_log_alpha = numpy.copy(log_alpha)
+    proj = numpy.dot(alpha,x)
+    old_proj = numpy.copy(proj)
+
+    new_obj = KL(y, proj)
+    y_over_proj = y/proj
+    grad = -numpy.dot(x, y_over_proj.transpose())
+    old_grad = numpy.copy(grad)
+
+    stepsize = 1
+    decreasing = False
+    repeat = False
+    gap = float('inf')
+
+    while 1:
+        eta = stepsize
+        old_obj = new_obj
+        old_alpha = numpy.copy(alpha)
+        old_log_alpha = numpy.copy(log_alpha)
+
+        old_proj = numpy.copy(proj)
+
+        it += 1
+        #take a step
+        log_alpha -= eta*grad
+
+        #normalize
+        log_alpha -= logsum_exp(log_alpha)
+
+        #compute new objective
+        alpha = numpy.exp(log_alpha)
+        proj = numpy.dot(alpha,x)
+        new_obj = KL(y, proj)
+        if new_obj < eps:
+            break
+
+        grad_dot_deltaAlpha = numpy.dot(grad, alpha - old_alpha)
+        assert (grad_dot_deltaAlpha <= 10**(-9))
+        if not new_obj <= old_obj + c1*stepsize*grad_dot_deltaAlpha: #sufficient decrease
+            stepsize /= 2.0 #reduce stepsize
+            if stepsize < 10**(-6):
+                break
+            alpha = old_alpha 
+            log_alpha = old_log_alpha
+            proj = old_proj
+            new_obj = old_obj
+            repeat = True
+            decreasing = True
+            continue
+
+        
+        #compute the new gradient
+        old_grad = numpy.copy(grad)
+        y_over_proj = y/proj
+        grad = -numpy.dot(x, y_over_proj)
+
+        if not numpy.dot(grad, alpha - old_alpha) >= c2*grad_dot_deltaAlpha and not decreasing: #curvature
+            stepsize *= 2.0 #increase stepsize
+            alpha = old_alpha
+            log_alpha = old_log_alpha
+            grad = old_grad
+            proj = old_proj
+            new_obj = old_obj
+            repeat = True
+            continue
+
+        decreasing= False
+        lam = numpy.copy(grad)
+        lam -= lam.min()
+        
+        gap = numpy.dot(alpha, lam)
+        convergence = gap
+        if (convergence < eps):
+            break
+
+    return alpha
+
+def fast_recover(y, X, w, anchors, XXT, initial_stepsize, epsilon):
+    k = len(anchors)
+    alpha = numpy.zeros(k)
+    gap = None
+    if w in anchors:
+        alpha[anchors.index(v)] = 1
+        it = -1
+        dist = 0
+        stepsize = 0
+
+    else: 
+        alpha = KLSolveExpGrad(y, x, epsilon)
+
+    if numpy.isnan(alpha).any():
+        alpha = ones(k)/k
+
+    return alpha
+        
+
+
+
+def computeA(cooccur, anchors, epsilon=2e-7):
+    Q = cooccur.copy()
+    v = Q.shape[0]
+    k = len(anchors)
+    A = numpy.zeros((v, k))
+
+    # store normalization constants
+    P_w = numpy.diag(Q.sum(axis=1))
+    for w in range(v):
+        if numpy.isnan(P_w[w, w]):
+            P_w[w, w] = 1e-16    
+
+    # Normalize rows of Q
+    for w in range(v):
+        if Q[w, :].sum() != 0:
+            Q[w, :] = Q[w, :] / Q[w, :].sum()
+
+    X = Q[anchors,:]
+    XXT = numpy.dot(X, X.transpose())
+
+    C = numpy.zeros((v, k))
+    for w in range(v):
+        y = Q[w, :]
+        alpha = fast_recover(y, X, w, anchors, XXT, initial_stepsize, epsilon)
+        C[w, :] = alpha 
+
+    # Use Bayes rule to compute topic matrix
+    A = numpy.dot(P_w, C)
+    
+    # Normalize columns
+    for i in range(k):
+        A[:, i] = A[:, i] / A[:, i].sum()
+
+
+    return numpy.array(A)
+
+
+
---
+++ search.py
@@ -0,0 +1,63 @@
+import numpy 
+import scipy.sparse
+import math
+from . import projection
+
+def row_normalize(Q):
+    Q_new = Q.copy()
+    row_sums = Q_new.sum(1)
+    for i in range(Q_new.shape[0]):
+        # numpy.seterr(divide='ignore', invalid='ignore')
+        if row_sums[i] != 0:
+            Q_new[i, :] = Q_new[i, :]/float(row_sums[i])
+    return Q_new
+
+def basis_vector(Q, i):
+    return Q[i]/numpy.sqrt(numpy.dot(Q[i], Q[i].T))
+
+def greedy_anchors(model, candidates, project_dim=1000):
+    Q_bar = row_normalize(model.cooccur)
+    Q_red = random_projection(Q_bar, project_dim, model.seed)
+
+    anchors = numpy.zeros(model.n_topics, dtype=numpy.int)
+    dim = Q_red.shape[1]
+    basis = numpy.zeros((model.n_topics - 1, dim))
+
+    # find p1 with farthest distance from origin
+    max_dist = 0
+    for w in candidates:
+        dist = numpy.dot(Q_red[w], Q_red[w].T)
+        if dist > max_dist:
+            max_dist = dist
+            anchors[0] = w
+
+    # let p1 be origin of both coordinate systems
+    for w in candidates:
+        Q_red[w] = Q_red[w] - Q_red[anchors[0]]
+
+    # find farthest point from p1 
+    max_dist = 0
+    for w in candidates:
+        dist = numpy.dot(Q_red[w], Q_red[w].T)
+        if dist > max_dist:
+            max_dist = dist
+            anchors[1] = w
+
+    basis[0] = basis_vector(Q_red, anchors[1])
+
+
+
+    # stabilized gram-schmidt which finds new anchor words to expand our subspace
+    for j in range(1, n_topics - 1):
+        max_dist = 0
+        for w in candidates:
+            Q_red[w] = Q_red[w] - numpy.dot(Q_red[w], basis[j-1]) * basis[j-1]
+            
+            dist = numpy.dot(Q_red[w], Q_red[w].T)
+            if dist > max_dist:
+                max_dist = dist
+                anchors[j+1] = w
+
+        basis[j] = basis_vector(Q_red, anchors[j+1])
+
+    return anchors
